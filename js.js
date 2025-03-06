3e281afd0a3caf2c80605599710983d20a19e603
Waleed Ammar <waleed.gamer173@gmail.com>
2025-03-06 19:06:10 +0400

    yay btn on work!

--- js.js
+++ js.js
@@ -1,33 +1,8 @@
 console.log(`STOP! YOU ARE NOT ALLOWED TO VIEW THE SOURCE CODE!\nIF YOU WANT TO SEE THE SOURCE CODE, PLEASE CONTACT THE OWNER OF THE WEBSITE!`);
-// function search() {
-//     console.log("Search button clicked");
-//     var input = document.getElementById("searchbar").value;
-//     var url = "https://www.google.com/search?q=" + encodeURIComponent(input);
-//     window.open(url, "_blank");
-// }
-
-// Ensure DOM is fully loaded before running scripts
-document.addEventListener("DOMContentLoaded", function() {
-    // Open search popup
-    document.getElementById("openSearch").addEventListener("click", function() {
-        document.getElementById("searchPopup").style.display = "block";
-    });
-
-    // Close search popup
-    document.getElementById("closeSearch").addEventListener("click", function() {
-        document.getElementById("searchPopup").style.display = "none";
-    });
-
-    // Search function (Example: alert input value)
-    document.getElementById("searchBtn").addEventListener("click", function() {
-        let query = document.getElementById("searchbar").value;
-        alert("You searched for: " + query);
-    });
-});
 
 function search() {
     console.log("Search button clicked");
-    var input = document.getElementById("searchbar").value;
+    var input = document.getElementById("searchbtn").value;
     var url = "https://www.google.com/search?q=" + encodeURIComponent(input);
     window.open(url, "_blank");
 }
