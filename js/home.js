ded2c576af49b2692e91c8f87ed48ffcba558669
Waleed Ammar <waleed.gamer173@gmail.com>
2025-04-09 22:21:38 +0400

    code fixes

--- js/home.js
+++ js/home.js
@@ -1,13 +1,20 @@
 window.addEventListener("load", function () {
     console.log("%cHome script connected", "background-color: blue; color: white; border-radius: 5px;");
-    // Get user data from local storage
+
+    // User from local storage
     const userData = JSON.parse(localStorage.getItem("user"));
+
+    // Define an array of greetings
+    const greetings = ["Hi", "Hello", "Welcome", "Bonjour", "Hola", "Greetings", "Hey there"];
+
+    // Randomly select a greeting
+    const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
 
     if (userData && typeof userData === "object") {
         // Ensure user data exists before setting textContent
-        document.getElementById("userGreetings").textContent = `Welcome ${userData.firstName}!` || "";
+        document.getElementById("userGreetings").textContent = `${randomGreeting} ${userData.firstName}!`;
     } else {
-        // If no user data is found, show a message
-        document.getElementById('userGreetings').textContent = "Welcome!"
+        // If no user data is found, show a default message
+        document.getElementById('userGreetings').textContent = `${randomGreeting}!`;
     }
 });
