63dd4849bd5c917b3df0c44be46fdc386e5fff39
Waleed Ammar <waleed.gamer173@gmail.com>
2025-04-15 14:02:57 +0400

    notes

--- js/notes.js
+++ js/notes.js
@@ -1,16 +1,3 @@
-// init Quill editor
-const quill = new Quill('#editor', {
-    theme: 'snow',
-    modules: {
-      toolbar: '#toolbar'
-    }
-  });
-  
-  // Example: Save notes when button is clicked
-  document.getElementById('saveNotes').addEventListener('click', () => {
-    const content = quill.root.innerHTML;
-    console.log('Saving content:', content);
-  
-    // TODO: Replace this with Firebase saving logic
-  });
-  
+function formatText(command) {
+    document.execCommand(command, false, null);
+}
