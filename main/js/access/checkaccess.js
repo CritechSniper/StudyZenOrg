5062f167dbee2152bbcf83133418bba082380874
Waleed Ammar <waleed.gamer173@gmail.com>
2025-06-29 11:46:03 +0400

    Locked Down

--- main/js/access/checkaccess.js
+++ main/js/access/checkaccess.js
@@ -1,11 +1,11 @@
-// const granted = localStorage.getItem("access") === "accessgranted24h";
-// const grantedTime = localStorage.getItem("accessTime");
-// const now = Date.now();
+const granted = localStorage.getItem("access") === "accessgranted24h";
+const grantedTime = localStorage.getItem("accessTime");
+const now = Date.now();
 
-// const twentyFourHours = 24 * 60 * 60 * 1000;
+const twentyFourHours = 24 * 60 * 60 * 1000;
 
-// if (!granted || !grantedTime || now - grantedTime > twentyFourHours) {
-//   localStorage.removeItem("access");
-//   localStorage.removeItem("accessTime");
-//   window.location.href = "access-denied.html"; // or login again
-// }
+if (!granted || !grantedTime || now - grantedTime > twentyFourHours) {
+  localStorage.removeItem("access");
+  localStorage.removeItem("accessTime");
+  window.location.href = "access-denied.html"; // or login again
+}
